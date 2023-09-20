a435100ccbe09bc210639cefbb7c9b45fff0a8ab
Tuomas Paulin <tuomas.paulin@polarsquad.com>
2023-09-20 12:44:28 +0300

    cdk: fix references and stackname

--- infra/bin/viestinvalityspalvelu.ts
+++ infra/bin/viestinvalityspalvelu.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { ViestinValitysStack } from '../lib/viestinvalitys-stack';
 
 const app = new cdk.App();
-new ViestinValitysStack(app, 'InfraStack', {
+new ViestinValitysStack(app, 'ViestinValitysStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
