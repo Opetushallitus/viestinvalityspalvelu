ad46f5c055510dfacb860a98bb96e345f8f65715
Santeri Korri <santeri.korri@knowit.fi>
2023-09-21 13:39:33 +0300

    send from lambda

--- vastaanotto/src/main/scala/fi/oph/viestinvalitus/App.java
+++ vastaanotto/src/main/scala/fi/oph/viestinvalitus/App.java
@@ -1,12 +1,28 @@
 package fi.oph.viestinvalitus;
 
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 public class App {
+
+  @Bean
+  public SQSService getSQSService(@Value("${localstack.endpoint}") String endpoint, @Value("${localstack.region}") String region, @Value("${localstack.accessKey}") String accessKey, @Value("${localstack.secretKey}") String secretKey, @Value("${localstack.queueUrl}") String queueUrl, @Value("${LOCALSTACK_HOSTNAME}") String localstackHostname) {
+    try {
+      queueUrl = new URIBuilder(queueUrl).setHost(localstackHostname).setPort(4566).build().toString();
+      endpoint = new URIBuilder(endpoint).setHost(localstackHostname).setPort(4566).build().toString();
+
+      return new SQSService(queueUrl, endpoint, region, accessKey, secretKey);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
--- vastaanotto/src/test/scala/fi/oph/viestinvalitus/junit.scala
+++ vastaanotto/src/test/scala/fi/oph/viestinvalitus/junit.scala
@@ -1,12 +1,14 @@
 package fi.oph.viestinvalitus
 
+import com.github.dockerjava.api.model.{ExposedPort, PortBinding}
+import com.github.dockerjava.api.model.Ports.Binding
 import org.apache.commons.io.{FileUtils, IOUtils}
 import org.apache.http.HttpResponse
 import org.apache.http.client.methods.{HttpPut, HttpUriRequest}
 import org.apache.http.client.utils.URIBuilder
 import org.apache.http.entity.ByteArrayEntity
 import org.apache.http.impl.client.HttpClientBuilder
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.{assertEquals, assertTrue}
 import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.junit.jupiter.api.{BeforeAll, BeforeEach, Test, TestInstance}
 import org.testcontainers.containers.GenericContainer
@@ -17,7 +19,9 @@ import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCrede
 import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.lambda.LambdaClient
-import software.amazon.awssdk.services.lambda.model.{CreateFunctionRequest, CreateFunctionResponse, CreateFunctionUrlConfigRequest, CreateFunctionUrlConfigResponse, FunctionCode, GetFunctionConfigurationRequest, GetFunctionRequest}
+import software.amazon.awssdk.services.lambda.model.{CreateFunctionRequest, CreateFunctionResponse, CreateFunctionUrlConfigRequest, CreateFunctionUrlConfigResponse, Environment, FunctionCode, GetFunctionConfigurationRequest, GetFunctionRequest}
+import software.amazon.awssdk.services.sqs.SqsClient
+import software.amazon.awssdk.services.sqs.model.{CreateQueueRequest, CreateQueueResponse, DeleteMessageRequest, ReceiveMessageRequest, ReceiveMessageResponse}
 
 import java.io.{File, FileInputStream}
 import java.net.URI
@@ -27,13 +31,32 @@ import java.nio.charset.StandardCharsets
 @TestInstance(Lifecycle.PER_CLASS)
 class AppTest {
 
-    @Container var localstack: LocalStackContainer = new LocalStackContainer(new DockerImageName("localstack/localstack:2.2.0")).withExposedPorts(4566)
+    @Container var localstack: LocalStackContainer = new LocalStackContainer(new DockerImageName("localstack/localstack:2.2.0"))
+      .withEnv("LAMBDA_DOCKER_FLAGS", "-p 127.0.0.1:5050:5050")
+      .withEnv("LAMBDA_DOCKER_NETWORK", "bridge")
+      .withEnv("LAMBDA_KEEPALIVE_MS", "0")
+      .withExposedPorts(4566)
 
     @BeforeAll def setUp(): Unit = {
       localstack.start()
     }
 
     @Test def testSimplePutAndGet(): Unit = {
+        val sqsClient: SqsClient = SqsClient
+          .builder()
+          .endpointOverride(localstack.getEndpoint())
+          .credentialsProvider(
+            StaticCredentialsProvider.create(
+              AwsBasicCredentials.create(localstack.getAccessKey(), localstack.getSecretKey())
+            )
+          )
+          .region(Region.of(localstack.getRegion()))
+          .build()
+
+        val createQueueResponse: CreateQueueResponse = sqsClient.createQueue(CreateQueueRequest.builder()
+          .queueName("viestinvalituspalvelu-vastaanotto-high")
+          .build())
+
         val lambdaClient: LambdaClient = LambdaClient
           .builder()
           .endpointOverride(localstack.getEndpoint())
@@ -45,6 +68,9 @@ class AppTest {
           .region(Region.of(localstack.getRegion()))
           .build();
 
+      val queueUrl: String = new URIBuilder(createQueueResponse.queueUrl()).setHost("localhost.localstack.cloud").setPort(4566).build().toString()
+      val endpoint: String = new URIBuilder(localstack.getEndpoint).setHost("localhost.localstack.cloud").setPort(4566).build().toString()
+
         val createFunctionResponse: CreateFunctionResponse = lambdaClient.createFunction(CreateFunctionRequest.builder()
           .functionName("vastaanotto")
           .code(FunctionCode.builder()
@@ -54,6 +80,15 @@ class AppTest {
           .runtime("java17")
           .timeout(1500)
           .role("arn:aws:iam::000000000000:role/lambda-role")
+          .environment(Environment.builder()
+            .variables(java.util.Map.of(
+              "localstack.endpoint", endpoint,
+              "localstack.region", localstack.getRegion,
+              "localstack.accessKey", localstack.getAccessKey,
+              "localstack.secretKey", localstack.getSecretKey,
+              "localstack.queueUrl", queueUrl,
+              "_JAVA_OPTIONS", "-Xshare:off -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=0.0.0.0:5050"))
+            .build())
           .build())
 
         lambdaClient.waiter().waitUntilFunctionActive(GetFunctionConfigurationRequest.builder().functionName("vastaanotto").build())
@@ -70,6 +105,20 @@ class AppTest {
       val response: HttpResponse = HttpClientBuilder.create().build().execute(request)
 
       System.out.println(IOUtils.toString(response.getEntity.getContent))
+
+      val receiveMessageResponse: ReceiveMessageResponse = sqsClient.receiveMessage(ReceiveMessageRequest.builder()
+        .queueUrl(createQueueResponse.queueUrl())
+        .build())
+
+      receiveMessageResponse.messages().forEach(message => {
+          assertEquals("test message", message.body())
+
+          sqsClient.deleteMessage(DeleteMessageRequest.builder()
+            .queueUrl(createQueueResponse.queueUrl())
+            .receiptHandle(message.receiptHandle())
+            .build())
+      })
+
     }
 }
 
