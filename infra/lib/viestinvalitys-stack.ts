03152c60e3cb4474340954977b62f1f7abc9b4f9
Santeri Korri <santeri.korri@knowit.fi>
2023-10-02 08:14:48 +0300

    OK-356 added lambda to infra

--- infra/lib/viestinvalitys-stack.ts
+++ infra/lib/viestinvalitys-stack.ts
@@ -1,18 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
-import { Function } from 'aws-cdk-lib/aws-lambda';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Duration} from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {Architecture, FunctionUrlAuthType} from 'aws-cdk-lib/aws-lambda';
+import {Construct} from 'constructs';
+import path = require("path");
 
 export class ViestinValitysStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+/*
+    // korkean prioriteetin jono
+    const highPriority = new sqs.Queue(this, 'priority-high', {
+       visibilityTimeout: cdk.Duration.seconds(300)
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // liitetiedostot
+    const attachmentBucket = new s3.Bucket(this, 'attachments');
+*/
+
+    const vastaanotto = new lambda.Function(this, 'vastaanotto', {
+      runtime: lambda.Runtime.JAVA_17,
+      handler: 'fi.oph.viestinvalitus.vastaanotto.LambdaHandler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../vastaanotto/target/vastaanotto-0.1-SNAPSHOT.jar')),
+      timeout: Duration.seconds(30),
+      memorySize: 512,
+      architecture: Architecture.X86_64
+    });
+
+    // Enable SnapStart
+    const cfnFunction = vastaanotto.node.defaultChild as lambda.CfnFunction;
+    cfnFunction.addPropertyOverride("SnapStart", { ApplyOn: "PublishedVersions" });
+
+    const version = vastaanotto.currentVersion;
+    const alias = new lambda.Alias(this, 'LambdaAlias', {
+      aliasName: 'Test',
+      version,
+    });
+
+    const versionUrl = alias.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE
+    })
   }
 }
