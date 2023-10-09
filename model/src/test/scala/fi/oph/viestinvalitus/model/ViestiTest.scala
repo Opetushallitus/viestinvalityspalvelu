0db53335205d906191b17517c7c934790092b355
Santeri Korri <santeri.korri@knowit.fi>
2023-10-09 15:15:23 +0300

    OK-356 separate model to own module

---
+++ model/src/test/scala/fi/oph/viestinvalitus/model/ViestiTest.scala
@@ -0,0 +1,37 @@
+package fi.oph.viestinvalitus.model
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import org.junit.jupiter.api.{Assertions, Test}
+
+import java.util.UUID
+
+@Test
+class ViestiTest {
+
+  @Test def testSerialization(): Unit = {
+
+    val viesti = Viesti(
+      otsikko = "testOtsikko",
+      sisalto = "testSisalto",
+      sisallonTyyppi = SisallonTyyppi.text,
+      kielet = Seq(Kieli.fi, Kieli.sv),
+      lahettavanVirkailijanOid = "testLahettajanOID",
+      lahettaja = Lahettaja("testLahettajaNimi", "testLahettajaOsoite"),
+      vastaanottajat = Seq(Vastaanottaja("testVastaanottajaNimi", "testVastaanOttajaOsoite")),
+      liitteet = Seq(UUID.randomUUID()),
+      lahettavaPalvelu = "testLahettavaPalvelu",
+      prioriteetti = Prioriteetti.normaali,
+      sailytysAika = 10,
+      kayttooikeusRajoitukset = Seq("testKayttooikeusRajoitus"),
+      metadata = Map("key" -> "value")
+    )
+
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+    val stringRepresentation = mapper.writeValueAsString(viesti);
+
+    val deserialized = mapper.readValue(stringRepresentation, classOf[Viesti])
+    Assertions.assertEquals(viesti, deserialized);
+  }
+}
