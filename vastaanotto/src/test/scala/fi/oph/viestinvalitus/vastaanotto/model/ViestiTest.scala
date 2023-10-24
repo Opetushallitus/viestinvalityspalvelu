5d74ad8319910fd2a354d63eeea67b181a6e7bd9
Santeri Korri <santeri.korri@knowit.fi>
2023-10-24 10:29:59 +0300

    OK-356 Päivitetty ObjectMapper-testejä

--- vastaanotto/src/test/scala/fi/oph/viestinvalitus/vastaanotto/model/ViestiTest.scala
+++ vastaanotto/src/test/scala/fi/oph/viestinvalitus/vastaanotto/model/ViestiTest.scala
@@ -3,17 +3,17 @@ package fi.oph.viestinvalitus.vastaanotto.model
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import fi.oph.viestinvalitus.vastaanotto.configuration.VastaanottoConfiguration
 import fi.oph.viestinvalitus.vastaanotto.model.{Lahettaja, Vastaanottaja, Viesti}
 import org.junit.jupiter.api.{Assertions, Test}
 
-import java.util.UUID
+import java.util.{Optional, UUID}
 
 @Test
 class ViestiTest {
 
-  @Test def testSerialization(): Unit = {
-
-    val viesti = Viesti(
+  def getExampleViesti(): Viesti =
+    Viesti(
       otsikko = "testOtsikko",
       sisalto = "testSisalto",
       sisallonTyyppi = "text",
@@ -30,12 +30,23 @@ class ViestiTest {
       metadata = java.util.Map.of("key", "value")
     )
 
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    mapper.registerModule(new Jdk8Module()) // tämä on java.util.Optional -kenttiä varten
-    val stringRepresentation = mapper.writeValueAsString(viesti);
+  def getObjectMapper(): ObjectMapper =
+    new VastaanottoConfiguration().objectMapper()
 
-    val deserialized = mapper.readValue(stringRepresentation, classOf[Viesti])
+  @Test def testSerializationRoundtrip(): Unit =
+    val mapper = getObjectMapper()
+
+    val viesti = getExampleViesti()
+    val json = mapper.writeValueAsString(viesti);
+    val deserialized = mapper.readValue(json, classOf[Viesti])
     Assertions.assertEquals(viesti, deserialized);
-  }
+
+  @Test def testIgnoreMissingProperties(): Unit =
+    val mapper = getObjectMapper()
+    val viesti = getExampleViesti().copy(lahettavanVirkailijanOid = Optional.empty)
+    val json = mapper.writeValueAsString(viesti).replaceAll("\"lahettavanVirkailijanOid\"\\s*:\\s*null\\s*,\\n?", "") // lähettäjän OID voi olla määrittelemätön
+
+    val deserialized = mapper.readValue(json, classOf[Viesti])
+    Assertions.assertEquals(viesti, deserialized);
+
 }
