34cc6b36a24e5a210bfa067b22c71bbf6fb35c72
Santeri Korri <santeri.korri@knowit.fi>
2023-11-06 09:05:58 +0200

    Lisätty tila liitteisiin

--- model/src/main/scala/fi/oph/viestinvalitys/model/Liitteet.scala
+++ model/src/main/scala/fi/oph/viestinvalitys/model/Liitteet.scala
@@ -1,15 +1,21 @@
 package fi.oph.viestinvalitys.model
 
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
 import slick.jdbc.PostgresProfile.api.*
 
 import java.util.UUID
 
-class Liitteet(tag: Tag) extends Table[(UUID, String, String, Int, String)](tag, "liitteet") {
+enum LiitteenTila:
+  case ODOTTAA, PUHDAS, SAASTUNUT, VIRHE
+
+class Liitteet(tag: Tag) extends Table[(UUID, String, String, Int, String, String)](tag, "liitteet") {
   def tunniste = column[UUID]("tunniste", O.PrimaryKey)
   def nimi = column[String]("nimi")
   def contentType = column[String]("content_type")
   def koko = column[Int]("koko")
   def omistaja = column[String]("omistaja")
+  def tila = column[String]("tila")
 
-  def * = (tunniste, nimi, contentType, koko, omistaja)
+  def * = (tunniste, nimi, contentType, koko, omistaja, tila)
 }
