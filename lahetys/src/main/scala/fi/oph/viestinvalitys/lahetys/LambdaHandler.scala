5bd461f51b4500b42e927d76b616c09ce76b4b12
Santeri Korri <santeri.korri@knowit.fi>
2023-11-07 16:40:03 +0200

    OK-356 Lisätty lähetyslambda ja invoke orkestroinnista

---
+++ lahetys/src/main/scala/fi/oph/viestinvalitys/lahetys/LambdaHandler.scala
@@ -0,0 +1,50 @@
+package fi.oph.viestinvalitys.lahetys
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException
+import com.amazonaws.serverless.proxy.model.{AwsProxyResponse, HttpApiV2ProxyRequest}
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler
+import com.amazonaws.services.lambda.runtime.events.{SNSEvent, SQSEvent}
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler, RequestStreamHandler}
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
+import fi.oph.viestinvalitys.db.dbUtil.getParameter
+import fi.oph.viestinvalitys.db.{awsUtil, dbUtil}
+import fi.oph.viestinvalitys.model.{Lahetykset, LiitteenTila, Liitteet, ViestinTila, Viestipohjat}
+import org.postgresql.ds.PGSimpleDataSource
+import org.slf4j.{Logger, LoggerFactory}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ConfigurableApplicationContext
+import slick.jdbc.PostgresProfile.api.*
+import slick.lifted.TableQuery
+import software.amazon.awssdk.auth.credentials.ContainerCredentialsProvider
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.{GetObjectRequest, GetObjectTaggingRequest}
+import software.amazon.awssdk.services.ssm.SsmClient
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest
+
+import java.time.Instant
+import java.util
+import java.util.UUID
+import java.util.stream.Collectors
+import scala.beans.BeanProperty
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.jdk.CollectionConverters.CollectionHasAsScala
+import scala.jdk.CollectionConverters.SeqHasAsJava
+import slick.jdbc.JdbcBackend
+import slick.jdbc.JdbcBackend.Database
+
+class LambdaHandler extends RequestHandler[java.util.List[UUID], Void] {
+
+  val LOG = LoggerFactory.getLogger(classOf[LambdaHandler]);
+
+  override def handleRequest(viestit: java.util.List[UUID], context: Context): Void = {
+    val db = dbUtil.getDatabase()
+    viestit.forEach(tunniste => {
+      LOG.info("Lähetetään viestiä: " + tunniste)
+      Thread.sleep(500)
+      dbUtil.paivitaViestinTila(tunniste, ViestinTila.LAHETETTY, db)
+    })
+    null
+  }
+}
