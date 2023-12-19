8398e4add62c6e603a84a95ec8e3e2330f37ad81
Santeri Korri <santeri.korri@knowit.fi>
2023-12-19 08:58:45 +0200

    Poistettu integraatio-moduuli

--- integraatio/src/test/scala/fi/oph/viestinvalitys/integraatio/OphPostgresContainer.scala
+++
@@ -1,6 +0,0 @@
-package fi.oph.viestinvalitys.integraatio
-
-import org.testcontainers.containers.PostgreSQLContainer
-
-class OphPostgresContainer(dockerImageName: String) extends PostgreSQLContainer[OphPostgresContainer](dockerImageName) {
-}
