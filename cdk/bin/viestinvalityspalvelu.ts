9d421d15bcb3711391217c8711fa2c9279253ce1
Santeri Korri <santeri.korri@knowit.fi>
2023-12-21 16:58:42 +0200

    Muutettu LahetysStack -> SovellusStack

--- cdk/bin/viestinvalityspalvelu.ts
+++ cdk/bin/viestinvalityspalvelu.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { LahetysStack } from '../lib/lahetys-stack';
+import { SovellusStack } from '../lib/sovellus-stack';
 import {PersistenssiStack} from "../lib/persistenssi-stack";
 import {LoadtestStack} from "../lib/loadtest-stack";
 
 const app = new cdk.App();
 const environmentName = app.node.tryGetContext("environment");
 
-new LahetysStack(app, 'LahetysStack', { // TODO: muuta
-    stackName: `${environmentName}-viestinvalitys-lahetys`,
+new SovellusStack(app, 'SovellusStack', {
+    stackName: `${environmentName}-viestinvalitys-sovellus`,
     environmentName: environmentName,
     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
