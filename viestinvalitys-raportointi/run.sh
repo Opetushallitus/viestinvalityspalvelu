3525a4eb8248b3027c8fb9fceffce1f786da92af
Marja Kari <marja.kari@amban.fi>
2024-03-18 13:13:27 +0200

    raportointikälin ympäristömuuttujat

--- viestinvalitys-raportointi/run.sh
+++ viestinvalitys-raportointi/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PORT=8080
-export VIESTINTAPALVELU_URL=https://viestinvalitys.hahtuvaopintopolku.fi
-export LOGIN_URL=https://viestinvalitys.hahtuvaopintopolku.fi/raportointi/login
+export PORT=$PORT
+export VIESTINTAPALVELU_URL="https://viestinvalitys.$ENVIRONMENT_NAME.fi"
+export LOGIN_URL=""$VIESTINTAPALVELU_URL/raportointi/login""
 export COOKIE_NAME=JSESSIONID
 
 node server.js
