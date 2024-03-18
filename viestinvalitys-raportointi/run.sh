2d36fb26f7ef3ba446c72e3a5afeff83d9871a5d
Marja Kari <marja.kari@amban.fi>
2024-03-18 13:15:52 +0200

    hipsufix

--- viestinvalitys-raportointi/run.sh
+++ viestinvalitys-raportointi/run.sh
@@ -2,7 +2,7 @@
 
 export PORT=$PORT
 export VIESTINTAPALVELU_URL="https://viestinvalitys.$ENVIRONMENT_NAME.fi"
-export LOGIN_URL=""$VIESTINTAPALVELU_URL/raportointi/login""
+export LOGIN_URL="$VIESTINTAPALVELU_URL/raportointi/login"
 export COOKIE_NAME=JSESSIONID
 
 node server.js
