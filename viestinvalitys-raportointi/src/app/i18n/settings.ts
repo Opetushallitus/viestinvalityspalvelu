c3fa17712fbc4d5191fdcb4298553ac62fad063b
Marja Kari <marja.kari@amban.fi>
2024-03-19 22:27:25 +0200

    kielistyksen alku

---
+++ viestinvalitys-raportointi/src/app/i18n/settings.ts
@@ -0,0 +1,17 @@
+import type {InitOptions} from 'i18next';
+
+export const FALLBACK_LOCALE = 'fi';
+export const supportedLocales = ['en', 'fi'] as const;
+export type Locales = (typeof supportedLocales)[number];
+
+// You can name the cookie to whatever you want
+export const LANGUAGE_COOKIE = 'preferred_language';
+
+export function getOptions(lang = FALLBACK_LOCALE): InitOptions {
+  return {
+    // debug: true, // Set to true to see console logs
+    supportedLngs: supportedLocales,
+    fallbackLng: FALLBACK_LOCALE,
+    lng: lang,
+  };
+}
