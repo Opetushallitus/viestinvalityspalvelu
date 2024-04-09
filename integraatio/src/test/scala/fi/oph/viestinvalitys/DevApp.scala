3d53aff53f09d4da5705b648d27da312cf695ded
Marja Kari <marja.kari@amban.fi>
2024-04-09 14:43:39 +0300

    fiksattu enviasetus testiin

--- integraatio/src/test/scala/fi/oph/viestinvalitys/DevApp.scala
+++ integraatio/src/test/scala/fi/oph/viestinvalitys/DevApp.scala
@@ -30,11 +30,11 @@ object DevApp {
     System.setProperty("cas-service.sendRenew", "false")
     System.setProperty("cas-service.key", "viestinvalityspalvelu")
     System.setProperty("web.url.cas", "https://virkailija.hahtuvaopintopolku.fi/cas")
-    System.setProperty("LOCAL_OPINTOPOLKU_DOMAIN", "hahtuvaopintopolku.fi")
+    //System.setProperty("LOCAL_OPINTOPOLKU_DOMAIN", "hahtuvaopintopolku.fi")
 
     System.setProperty("kayttooikeus-service.userDetails.byUsername", "https://virkailija.hahtuvaopintopolku.fi/kayttooikeus-service/userDetails/$1")
 
-    //System.setProperty("host.virkailija", "virkailija.hahtuvaopintopolku.fi")
+    System.setProperty("host.virkailija", "virkailija.hahtuvaopintopolku.fi")
     // swagger
     System.setProperty("springdoc.api-docs.path", "/openapi/v3/api-docs")
     System.setProperty("springdoc.swagger-ui.path", "/static/swagger-ui/index.html")
