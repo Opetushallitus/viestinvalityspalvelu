f1559fd505a087b8d748b603672a390071d0be2f
Marja Kari <marja.kari@amban.fi>
2024-04-17 15:33:02 +0300

    lisätty organisaatiokomponentti

--- viestinvalitys-raportointi/src/app/components/HtmlMessage.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import DOMPurify from 'dompurify';
-
-export default function HtmlMessage(message: string) {
-  return <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(message) }} />;
-}
---
+++ viestinvalitys-raportointi/src/app/components/OrganisaatioHierarkia.tsx
@@ -0,0 +1,77 @@
+'use client';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { TreeItem, TreeView } from '@mui/x-tree-view';
+import { Organisaatio } from '../lib/types';
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+} from '@mui/material';
+
+const OrganisaatioHierarkia = ({
+  organisaatiot,
+  selectedOid,
+  expandedOids,
+  handleSelect,
+  handleChange,
+  handleToggle,
+}: {
+  organisaatiot: Organisaatio[];
+  selectedOid: string | undefined;
+  expandedOids: string[];
+  handleSelect: any;
+  handleChange: any;
+  handleToggle: any;
+}) => {
+  const renderTree = (org: Organisaatio) => {
+    if (!org) {
+      return null;
+    }
+    return (
+      <TreeItem
+        key={org.oid}
+        nodeId={org.oid}
+        label={
+          <FormControl>
+            <FormControlLabel
+              label={org.nimi.fi}
+              control={
+                <Radio
+                  checked={selectedOid === org.oid}
+                  name="organisaatio"
+                  value={org.oid}
+                  onChange={e => {
+                    handleChange(e);
+                  }}
+                />
+              }
+            />
+          </FormControl>
+        }
+      >
+        {Array.isArray(org.children)
+          ? org.children.map(node => renderTree(node))
+          : null}
+      </TreeItem>
+    );
+  };
+
+  return (
+    <>
+      <TreeView
+        aria-label="organisaatiot"
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpandIcon={<ChevronRightIcon />}
+        onNodeSelect={handleSelect}
+        onNodeToggle={handleToggle}
+        selected={selectedOid}
+        expanded={expandedOids}
+      >
+        {organisaatiot.map(org => renderTree(org))}
+      </TreeView>
+    </>
+  );
+};
+
+export default OrganisaatioHierarkia;
