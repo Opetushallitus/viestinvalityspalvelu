fe0dc63ce5bfdaec526a8f61006c4a0463278aaf
Marja Kari <marja.kari@amban.fi>
2024-06-06 13:53:16 +0300

    osoitekonffit toimimaan myös qa:lle

--- viestinvalitys-raportointi/run.sh
+++ viestinvalitys-raportointi/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-export PORT=$PORT
-export VIESTINTAPALVELU_URL="https://viestinvalitys.$ENVIRONMENT_NAME.fi"
+export PORT=8080
+if [$ENVIRONMENT_NAME == "pallero"] then
+  export VIESTINTAPALVELU_URL="https://viestinvalitys.testiopintopolku.fi"
+else
+  export VIESTINTAPALVELU_URL="https://viestinvalitys.$ENVIRONMENT_NAME.fi"
+fi
 export LOGIN_URL="$VIESTINTAPALVELU_URL/raportointi/login"
 export COOKIE_NAME=JSESSIONID
 
