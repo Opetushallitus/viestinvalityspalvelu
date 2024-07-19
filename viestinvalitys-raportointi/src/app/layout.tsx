4fb6d1434e4124d12b2d2cb54bb81e639cd2fa52
Marja Kari <marja.kari@amban.fi>
2024-07-19 09:23:17 +0300

    OK-444 poistettu fontti

--- viestinvalitys-raportointi/src/app/layout.tsx
+++ viestinvalitys-raportointi/src/app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from 'next';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import './globals.css';
-import { Roboto } from 'next/font/google';
 import NavAppBar from './components/NavAppBar';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import theme from './theme';
 import ReactQueryClientProvider from './components/react-query-client-provider';
 import { PageLayout } from './components/PageLayout';
 
-const roboto = Roboto({
-  weight: ['400', '500'],
-  display: 'swap',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Viestinvälityspalvelun raportointi',
@@ -25,7 +19,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="fi" className={roboto.className}>
+    <html lang="fi">
       <body>
         <AppRouterCacheProvider>
           <ReactQueryClientProvider>
