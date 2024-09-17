0aad41f97af0fb9ea745435dda7e1f735fa1c29f
Marja Kari <marja.kari@amban.fi>
2024-09-17 16:11:52 +0300

    OK-436 fiksattu kantamigraatio ja importit

--- shared/src/main/resources/flyway/V202409120000__create_lahettavapalvelu_index.sql
+++ shared/src/main/resources/flyway/V202409120000__create_lahettavapalvelu_index.sql
@@ -1,2 +1,2 @@
 -- lista lähettävistä palveluista muodostetaan toistaiseksi lähetyksien tiedoista, palvelu cachettaa
-create index lahetykset_lahettavapalvelu_index on lahetykset (lahettavapalvelu asc);
+CREATE INDEX IF NOT EXISTS lahetykset_lahettavapalvelu_index on lahetykset (lahettavapalvelu asc);
