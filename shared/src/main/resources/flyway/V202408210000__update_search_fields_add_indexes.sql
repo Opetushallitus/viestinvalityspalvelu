29c5b984cc6f6c34c4a5e58e9c79202707373dcc
Marja Kari <marja.kari@amban.fi>
2024-09-23 14:42:46 +0300

    OK-435 eipäs muutellakaan kantamigraatioita vaan tehdään uusi migraatio

--- shared/src/main/resources/flyway/V202408210000__update_search_fields_add_indexes.sql
+++ shared/src/main/resources/flyway/V202408210000__update_search_fields_add_indexes.sql
@@ -3,7 +3,7 @@ UPDATE viestit SET haku_sisalto=to_tsvector('finnish', sisalto) || to_tsvector('
 UPDATE viestit SET haku_otsikko=to_tsvector('finnish', otsikko) || to_tsvector('swedish', otsikko) || to_tsvector('english', otsikko) || to_tsvector('simple', otsikko) WHERE haku_otsikko IS NULL;
 UPDATE viestit SET haku_kayttooikeudet=(SELECT array_agg(kayttooikeus_tunniste) FROM viestit_kayttooikeudet WHERE tunniste=viesti_tunniste) WHERE haku_kayttooikeudet IS NULL;
 UPDATE viestit SET haku_vastaanottajat=(SELECT array_agg(sahkopostiosoite) FROM vastaanottajat WHERE viestit.tunniste=viesti_tunniste) WHERE haku_vastaanottajat IS NULL;
-UPDATE viestit SET haku_lahettaja=(SELECT lahettavanvirkailijanoid FROM lahetykset WHERE lahetykset.tunniste=lahetys_tunniste) WHERE haku_lahettaja IS NULL;
+UPDATE viestit SET haku_lahettaja=(SELECT lahettajansahkoposti FROM lahetykset WHERE lahetykset.tunniste=lahetys_tunniste) WHERE haku_lahettaja IS NULL;
 UPDATE viestit SET haku_metadata=(SELECT array_agg(avain || ':' || arvo) FROM metadata WHERE tunniste=viesti_tunniste) WHERE haku_metadata IS NULL;
 UPDATE viestit SET haku_metadata='{}'::varchar[] WHERE haku_metadata IS NULL;
 UPDATE viestit SET haku_lahettavapalvelu=(SELECT lahettavapalvelu FROM lahetykset WHERE lahetykset.tunniste=lahetys_tunniste) WHERE haku_lahettavapalvelu IS NULL;
@@ -18,6 +18,7 @@ ALTER TABLE viestit ALTER COLUMN haku_sisalto SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_otsikko SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_kayttooikeudet SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_vastaanottajat SET NOT NULL;
+ALTER TABLE viestit ALTER COLUMN haku_lahettaja SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_metadata SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_lahettavapalvelu SET NOT NULL;
 ALTER TABLE viestit ALTER COLUMN haku_organisaatiot SET NOT NULL;
