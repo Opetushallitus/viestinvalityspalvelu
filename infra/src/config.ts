e38cb0ddc24ca7815f2bab3aac33fb66f81930a4
Timo Suomela <timo.suomela@reaktor.com>
2025-02-21 12:27:51 +0200

    OPHYK-482 Add env specific configuration file

---
+++ infra/src/config.ts
@@ -0,0 +1,42 @@
+const environments = ["hahtuva", "dev", "qa", "prod"] as const;
+type EnvironmentName = (typeof environments)[number];
+
+export type Config = {
+  vpcCidr: string;
+};
+
+export function getEnvironment(): EnvironmentName {
+  const env = process.env.ENV;
+  if (!env) {
+    throw new Error("ENV environment variable is not set");
+  }
+  if (!contains(environments, env)) {
+    throw new Error(`Invalid environment name: ${env}`);
+  }
+  return env as EnvironmentName;
+}
+
+function contains(arr: readonly string[], value: string): boolean {
+  return arr.includes(value);
+}
+
+export function getConfig(): Config {
+  const env = getEnvironment();
+  return { hahtuva, dev, qa, prod }[env];
+}
+
+export const hahtuva: Config = {
+  vpcCidr: "10.22.0.0/18",
+};
+
+export const dev: Config = {
+  vpcCidr: "10.22.64.0/18",
+};
+
+export const qa: Config = {
+  vpcCidr: "10.22.128.0/18",
+};
+
+export const prod: Config = {
+  vpcCidr: "10.22.192.0/18",
+};
