3700ebe35eccb390b2f3be92416ca586c7663621
Timo Suomela <timo.suomela@reaktor.com>
2025-03-05 09:46:28 +0200

    OPHYK-482 Send SES events to queue

--- infra/src/ses.ts
+++ infra/src/ses.ts
@@ -2,8 +2,13 @@ import * as cdk from "aws-cdk-lib";
 import * as constructs from "constructs";
 import * as ses from "aws-cdk-lib/aws-ses";
 import * as route53 from "aws-cdk-lib/aws-route53";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as sns_subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
 
 export class SESStack extends cdk.Stack {
+  readonly monitorointiQueue: sqs.Queue;
+
   constructor(
     scope: constructs.Construct,
     id: string,
@@ -12,8 +17,68 @@ export class SESStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
+    this.monitorointiQueue = this.createMonitorointiQueue();
+    const monitorointiTopic = this.createMonitorointiTopic(
+      this.monitorointiQueue,
+    );
+    const configurationSet = this.createConfigurationSet(monitorointiTopic);
     new ses.EmailIdentity(this, "EmailIdentity", {
       identity: ses.Identity.publicHostedZone(hostedZone),
+      configurationSet: configurationSet,
+    });
+  }
+
+  private createConfigurationSet(monitorointiTopic: sns.ITopic) {
+    const configurationSet = new ses.ConfigurationSet(
+      this,
+      "ConfigurationSet",
+      {
+        configurationSetName: "viestinvalityspalvelu",
+      },
+    );
+    configurationSet.addEventDestination("EventDestination", {
+      destination: ses.EventDestination.snsTopic(monitorointiTopic),
+      enabled: true,
+      events: [
+        ses.EmailSendingEvent.SEND,
+        ses.EmailSendingEvent.REJECT,
+        ses.EmailSendingEvent.BOUNCE,
+        ses.EmailSendingEvent.COMPLAINT,
+        ses.EmailSendingEvent.DELIVERY,
+        ses.EmailSendingEvent.DELIVERY_DELAY,
+      ],
+    });
+    return configurationSet;
+  }
+
+  private createMonitorointiTopic(monitorointiQueue: sqs.Queue) {
+    const monitorointiTopic = new sns.Topic(
+      this,
+      "viestinvalityspalvelu-ses-monitorointi",
+      {
+        enforceSSL: true,
+      },
+    );
+    monitorointiTopic.addSubscription(
+      new sns_subscriptions.SqsSubscription(monitorointiQueue),
+    );
+    return monitorointiTopic;
+  }
+
+  private createMonitorointiQueue() {
+    const monitorointiDLQ = new sqs.Queue(this, "MonitorointiDLQ", {
+      queueName: "viestinvalityspalvelu-ses-monitorointiDlq",
+      retentionPeriod: cdk.Duration.days(14),
+      enforceSSL: true,
+    });
+    return new sqs.Queue(this, "MonitorointiQueue", {
+      queueName: "viestinvalityspalvelu-ses-monitorointi",
+      visibilityTimeout: cdk.Duration.seconds(60),
+      enforceSSL: true,
+      deadLetterQueue: {
+        maxReceiveCount: 3,
+        queue: monitorointiDLQ,
+      },
     });
   }
 }
