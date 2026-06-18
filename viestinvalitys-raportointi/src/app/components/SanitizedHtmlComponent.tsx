96bcffd3380947c8ce75c3c6be93e58768edce8f
Henry Heikkinen <henry.heikkinen@reaktor.com>
2026-06-18 13:37:09 +0300

    OPHYK-1362 OPHYK-1007 Sanitize html only in the browser to avoid SSR error
    This error has likely appeared after OPHYK-1007 and while I'm not at all
    sure it is relevant to issue OPHYK-1362 here is a fix just to rule it
    out.

--- viestinvalitys-raportointi/src/app/components/SanitizedHtmlComponent.tsx
+++ viestinvalitys-raportointi/src/app/components/SanitizedHtmlComponent.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import DOMPurify from 'dompurify';
+import { useEffect, useState } from 'react';
 
 export const SanitizedHtml = ({ html }: { html: string }) => {
+  const [sanitizedHtml, setSanitizedHtml] = useState('');
+
+  useEffect(() => {
+    setSanitizedHtml(DOMPurify.sanitize(html));
+  }, [html]);
+
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: DOMPurify.sanitize(html),
+        __html: sanitizedHtml,
       }}
     />
   );
