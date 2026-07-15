606361b7f56d16d6768c778fa354c0fbe60a25d4
Otto Uusihakala <otto.uusihakala@gmail.com>
2026-07-15 18:35:43 +0300

    OPHYK-1379 script for running viestinvalitys-service tests

---
+++ scripts/ci/run-viestinvalitys-service-tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+source "$( dirname "${BASH_SOURCE[0]}" )/../lib/common-functions.sh"
+
+trap cleanup EXIT INT QUIT TERM
+
+function main {
+  select_java_version "21"
+
+  cd "${repo}"
+
+  if is_running_on_ci; then
+    docker compose up -d
+  fi
+
+  cd "${repo}/viestinvalitys-service"
+
+  wait_for_container_to_be_healthy viestinvalitys-test-postgres
+  wait_for_container_to_be_healthy viestinvalitys-keycloak
+
+  ../mvnw --batch-mode clean install
+}
+
+function cleanup {
+  if is_running_on_ci; then
+    docker compose down
+  fi
+}
+
+main "$@"
