e2653c90e61f4778460ebdea57f9487151f6e6b6
Otto Uusihakala <otto.uusihakala@gmail.com>
2026-07-20 16:59:33 +0300

    OPHYK-1379 removed unnecessary HealthController tests

--- viestinvalitys-service/src/test/java/fi/vm/sade/viestinvalitys/resource/HealthControllerTest.java
+++ viestinvalitys-service/src/test/java/fi/vm/sade/viestinvalitys/resource/HealthControllerTest.java
@@ -1,7 +1,6 @@
 package fi.vm.sade.viestinvalitys.resource;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import fi.vm.sade.viestinvalitys.ViestinvalitysServiceApiTest;
@@ -12,18 +11,5 @@ class HealthControllerTest extends ViestinvalitysServiceApiTest {
   @Test
   void actuatorHealthIsPublic() throws Exception {
     mvc.perform(get("/actuator/health")).andExpect(status().isOk());
-  }
-
-  @Test
-  void healthcheckRequiresAuthentication() throws Exception {
-    mvc.perform(get("/raportointi/v1/healthcheck")).andExpect(status().isUnauthorized());
-  }
-
-  @Test
-  @UserKatselijaRaportoija
-  void healthcheckReturnsOkForAuthenticatedUser() throws Exception {
-    mvc.perform(get("/raportointi/v1/healthcheck"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.status").value("ok"));
   }
 }
